9b21c2c2aa3f931df6c0b7792dcb2924d455f0d6
KuanToto <stephencheng91@gmail.com>
2019-07-16 18:35:02 -0700

    Updates

---
+++ precache-manifest.ce68f7402e9af96ef631b664333e1242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9ede1d9de02473ecf82c8e67775ce0",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "a55eaca6eaa6744e00e6",
+    "url": "/clickygame/static/css/main.9924043f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a55eaca6eaa6744e00e6",
+    "url": "/clickygame/static/js/main.a07ce4e8.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
